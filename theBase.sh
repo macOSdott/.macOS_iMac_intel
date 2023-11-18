8f00523a83d08c7018fc00e1c7fdec8c62d97e8b
macOSdott <65959638+macOSdott@users.noreply.github.com>
2023-11-18 10:34:22 +0100

    settings for activity monitor do not work anymore

--- theBase.sh
+++ theBase.sh
@@ -519,20 +519,6 @@ defaults write com.apple.assistant.support 'Assistant Enabled' 0
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
 ###############################################################################
-# Activity Monitor                                                            #
-###############################################################################
-
-# Show the main window when launching Activity Monitor
-defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
-
-# Show all processes in Activity Monitor
-defaults write com.apple.ActivityMonitor ShowCategory -int 0
-
-# Sort Activity Monitor results by CPU usage
-defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
-defaults write com.apple.ActivityMonitor SortDirection -int 0
-
-###############################################################################
 # Terminal	                                                              #
 ###############################################################################
 
